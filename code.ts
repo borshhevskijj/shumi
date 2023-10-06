9c4499c2f63afdcab0bbee547b5d8fce9701c743
lemonsqueezy <borshhevskijj98@mail.ru>
2023-10-06 22:57:56 +0300

    testing feature

--- code.ts
+++ code.ts
@@ -7,32 +7,19 @@ figma.notify('Select the element to apply the noise effect to')
 figma.closePlugin()
 }
 
-
-
 const MAX_SAFE_SIZE = 4000
 const isValidImageSize=(width:number,height:number)=> {
 return width <= MAX_SAFE_SIZE && height <= MAX_SAFE_SIZE;
 }
 
+const chunkSize = 1500;
+function splitBlockIntoChunks(blockWidth:number,blockHeight:number) {
+  const chunkWidthAmount = Math.ceil(blockWidth / chunkSize);
+  const chunkHeightAmount = Math.ceil(blockHeight / chunkSize);
 
-function getParts(size:number) {
-  const numChunks = Math.ceil(size / MAX_SAFE_SIZE);
-  
-  // Рассчитываем размер каждой части
-  const chunkHeight = Math.floor(size / numChunks);
-  
-  // Создаем массив для хранения размеров частей
-  const chunks = Array(numChunks).fill(chunkHeight);
-  console.log({numChunks,chunkHeight,chunks});
-  return {height:chunkHeight,chunks}
+
+  return {chunkWidthAmount,chunkHeightAmount}
 }
-
-// console.log(getParts(12000),3);
-// console.log(getParts(8000),2);
-// console.log(getParts(4000),1);
-// console.log(getParts(12345));
-
-
 
 
 const createNodeOfType = (node:SceneNode) => {
@@ -64,30 +51,54 @@ if ('cornerRadius' in node &&
 createdNode.name = 'noiseEffect';
 createdNode.x = x;
 createdNode.y = y;
-createdNode.resize(width, height);
+// createdNode.resize(width, height);
 
 return createdNode;
 };
 
 
+type sizes = {
+  width:number|null
+  height:number|null
+}
   for(const node of nodes){
     const {width,height} = node
-
-    figma.ui.postMessage({width,height}) 
+    const {chunkWidthAmount,chunkHeightAmount} = splitBlockIntoChunks(width,height)
+    let sizes:sizes ={
+      width:null,
+      height:null
+    }
+    if (width <= chunkSize) {
+      sizes.width = width
+    }
+    else{
+      sizes.width = chunkSize
+    }
+     if(height <=chunkSize){
+      sizes.height = height
+    }
+    else{
+       sizes.height = chunkSize
+     }
+    //  console.log(sizes,'sizes');
+     
+    figma.ui.postMessage(sizes) 
   }
 
 figma.ui.onmessage = async (message) => {
 const {pngBytes,mixBlendMode}= message
     try {
       for(const node of nodes){
+
         const {width,height} = node
-        if (!isValidImageSize(width,height)) {
-          figma.notify(`For better performance, try to keep element size under ${MAX_SAFE_SIZE}px`)
-        }
+        // if (!isValidImageSize(width,height)) {
+        //   figma.notify(`For better performance, try to keep element size under ${MAX_SAFE_SIZE}px`)
+        // }
+        const {chunkWidthAmount,chunkHeightAmount} = splitBlockIntoChunks(width,height)
+        // console.log(chunkWidthAmount,chunkHeightAmount,'chunkWidthAmount,chunkHeightAmount');
+        
         const image= figma.createImage(pngBytes)
-
         const element = createNodeOfType(node)
-
         element.fills = [
           {
             type: 'IMAGE',
@@ -96,12 +107,91 @@ const {pngBytes,mixBlendMode}= message
             blendMode:mixBlendMode,
           }
         ]
-    element.resize(width,height)
+        // __
+        const parent = node.parent || figma.currentPage
+        
+        const chunks =[node]
+        
+        // let posX = node.x
+        // let posY = node.y
+        // const bottomPos = node.y + node.height - chunkSize
+        element.resize(chunkSize,chunkSize)
+
+
+
+        // for (let i = 0; i < chunkWidthAmount + 1; i++) {
+        //   const elementClone = element.clone()
+        //   elementClone.x = posX
+        //   posX += chunkSize
+        //   chunks.push(elementClone)
+
+        //   if (i === chunkWidthAmount) {
+        //     console.log('i === chunkWidthAmount');
+            
+        //     if (bottomPos <= posY) {
+        //       console.log('bottomPos <= posY');
+        //     }
+        //     i=0
+        //     posX = node.x
+        //     elementClone.y = posY
+        //     posY += chunkSize 
+        //     // if тут описать случай при котором нужно завершить цикл (когда posY будет больше или равер node.y )
+        //   }
+        // }
+
+        let posX = node.x
+        let posY = node.y
+        const nodeBottomYpos = node.y + node.height - chunkSize
+        const nodeBottomXpos = node.x + node.width - chunkSize
+        console.log({nodeBottomYpos,nodeBottomXpos});
+        
+        // const qwe = (nodeY:number,nodeX:number,currentPosX:number,currentPosY:number)=>{
+        //   let isBiggerCurrentPosX:boolean
+        //   let isBiggerCurrentPosY:boolean
+          
+        //   if (nodeX<0 && currentPosX <= nodeX) {
+        //     isBiggerCurrentPosX= true
+        //   }
+        //   else {
+        //     isBiggerCurrentPosX= false
+        //   }
+
+        //   if (nodeY<0) {
+             
+        //   }
+        //   else if (nodeY >=0) {
+        //   }
+        //   // return {isBiggerPosX,isBiggerPosY}
+        // }
+
+        let heightCounter =0
+        for (let chunkCount = 0; chunkCount <= chunkWidthAmount; chunkCount++) {
+          if (chunkCount === chunkWidthAmount) {
+            chunkCount=0
+            posX = node.x
+            posY+=chunkSize
+            heightCounter++
+          }
+          if (heightCounter === chunkHeightAmount) {
+              break
+          }
+          const elementClone = element.clone()
+          elementClone.x = posX
+          elementClone.y = posY
+          chunks.push(elementClone)
+          posX += chunkSize
+
+        }
+     
+
+        // __
+
+    // element.resize(width,height)
     
-    const parent = node.parent || figma.currentPage
-    parent.appendChild(element)
+    // const parent = node.parent || figma.currentPage
+    // parent.appendChild(element)
   
-    const group = figma.group([node,element], parent)
+    const group = figma.group(chunks, parent)
     group.name = `${node.name}_${element.name}`
     
   }
